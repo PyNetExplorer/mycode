1cfd5c93a5a42336945acc58971ad0b063bf69cc
PyNetExplorer <katyaportland@gmail.com>
2024-03-20 20:45:41 +0000

    Challenge with input and print

---
+++ challenge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+name = input("What is your name?\n") #ask for a name
+
+day = input("What day of the week it is?\n") #ask for day of the week
+
+import time #to use sleep function
+time.sleep(1) #sleep for 1 second
+
+print ("Hello, " + name + "! Happy " + day + "!") #print statement including user inputs name and day
+
+time.sleep (1) #sleep for 1 second
+
