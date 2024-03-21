f2bf8ee822378c722a38cb6dac13f692c8fcb57f
PyNetExplorer <katyaportland@gmail.com>
2024-03-21 19:49:28 +0000

    Mixed lists

---
+++ list_challenge01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# make a list
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# print only IP adresses
+print ("IP adresses: " + iplist[3] + ", and " + iplist[4])
+
