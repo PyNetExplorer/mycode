7aeed8a4b81a38196b8426b705bbbb21be59f9ea
PyNetExplorer <katyaportland@gmail.com>
2024-03-25 20:48:58 +0000

    Learning about input function

--- lab_input/iptaker02.py
+++ lab_input/iptaker02.py
@@ -5,13 +5,17 @@
 def main():
 
     # collect string input from the user
-    user_input = input("Please enter an IPv4 IP address:")
+    user_input = input("Please enter an IPv4 IP address:\n")
     
-    ## the line below creates a single string that is passed to print()
+    # the line below creates a single string that is passed to print()
     # print("You told me the IPv4 address is: " + user_input)
     
-    ## print() can be given a series of objects separated by a comma
+    # print() can be given a series of objects separated by a comma
     print("You told me the IPv4 address is:", user_input)
+
+    #collect and print vendor name
+    vendor_name = input ("Please enter your vendor name:\n")
+    print("You told me your vendor name is: " + vendor_name)
 
 main()
 
