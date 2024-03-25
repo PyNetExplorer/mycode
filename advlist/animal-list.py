e60aa2eb1afd7b4728921deaccc8f8e4f4e67c46
PyNetExplorer <katyaportland@gmail.com>
2024-03-25 20:04:22 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def main():
+    # create animal list
+    animals = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+    # Join the elements of the list with spaces
+    output = ' '.join(animals)
+    
+    # display animals
+    print(output)
+
+main()
+
