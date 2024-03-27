c329dcc51cf6149c91c8be09faf2fd3dafa04d68
PyNetExplorer <katyaportland@gmail.com>
2024-03-27 18:35:51 +0000

    Dictionary challenge

---
+++ dictionaries_challenge.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import time
+
+marvelchars = {
+    "Starlord": {"real name": "peter quill", "powers": "dance moves", "archenemy": "Thanos"},
+    "Mystique": {"real name": "raven darkholme", "powers": "shape shifter", "archenemy": "Professor X"},
+    "Hulk": {"real name": "bruce banner", "powers": "super strength", "archenemy": "adrenaline"}
+}
+
+while True:
+    char_name = input('Which character do you want to know about? (Starlord, Mystique, Hulk)\nType here: ').lower()
+    time.sleep(1)
+
+    char_stat = input('Great choice! What statistic do you want to know about? (real name, powers, archenemy)\nType here: ')
+    time.sleep(1)
+   
+    
+    value = marvelchars.get(char_name).get(char_stat)
+    time.sleep(1)
+    
+    print("Let's go!")
+    time.sleep(1)
+    print('3')
+    time.sleep(1)
+    print('2')
+    time.sleep(1)
+    print('1')
+    time.sleep(1)
+
+        
+    print(char_name + "'s " + char_stat + " is: " + value.title())
+    
+    # Ask user if they want to continue
+    choice = input("Do you want to continue? (yes/no): ").lower()
+    if choice != 'yes':
+        break
+
