5a5531bb3fdf423f3e895ac4f1b45c0b3203a91a
PyNetExplorer <katyaportland@gmail.com>
2024-03-29 17:09:54 +0000

    Linux Project

---
+++ main_project.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import tkinter as tk
+
+# Create the main window
+window = tk.Tk()
+
+# Set the title of the window
+window.title("Linux Configuration")
+
+# Set the dimensions of the window (width x height)
+window.geometry("1000x600")
+
+# Add widgets, such as buttons, labels, etc., here if needed
+
+# Run the Tkinter event loop
+window.mainloop()
